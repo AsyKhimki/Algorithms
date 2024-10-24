d8a26a07836a9db5145a81c6d22c6b571682ae5f
AsyKhimki <36882894+AsyKhimki@users.noreply.github.com>
2024-10-24 21:23:00 +0200

    Update 88_merge_sorted_array.cpp

--- LeetCode/88_merge_sorted_array.cpp
+++ LeetCode/88_merge_sorted_array.cpp
@@ -11,32 +11,29 @@ public:
             throw std::invalid_argument("nums1 does not have enough space to hold both arrays.");
         }
         
-        // Edge Case: If nums1 has no elements (m == 0), just copy nums2 into nums1
-        if (m == 0) {
-            std::copy(nums2.begin(), nums2.end(), nums1.begin());
+       // Edge Cases ----------------------------------------->
+        if(n == 0)
             return;
+         
+        // Main Loop ------------------------------------------>
+        int currentIndex = n+m-1;
+        int idx1{m-1}, idx2{n-1};
+        
+        // Start from the end of nums1
+        while(idx1 >= 0 && idx2 >= 0){
+            
+            if(nums1[idx1] > nums2[idx2]){
+               nums1[currentIndex--] = nums1[idx1--];
+            } else {
+               nums1[currentIndex--] = nums2[idx2--];
+            }
         }
         
-        // Main Loop --------------------------------------------->
+        while(idx2>=0){
+            nums1[idx2] = nums2[idx2];
+            idx2--;
+        }
         
-        int idx_last = n + m - 1;  // Index for the last position to fill in nums1
-        int idx1 = m - 1;          // Last element of the actual data in nums1
-        int idx2 = n - 1;          // Last element of nums2
-       
-
-        while(idx1 >= 0 && idx2 >= 0){
-
-            if(nums1[idx1] > nums2[idx2]){
-                nums1[idx_last--] = nums1[idx1--]; 
-            } else {
-                nums1[idx_last--] = nums2[idx2--];
-            } 
-        } // for
-        
-        // If there are still elements in nums2, copy them over
-        while(idx2 >= 0){
-            nums1[idx_last--]=nums2[idx2--];
-        } // while
-    
+        return;
     }
 };
